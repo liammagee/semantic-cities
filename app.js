dd99275df7314fc71dfbe64e4923a281f966385f
Liam Magee <liam.magee@gmail.com>
2013-11-09 13:32:03 +1100

    trivial change

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,35 +3,30 @@ google.maps.visualRefresh = true;
 
 var geocoder;
 var infowindow = new google.maps.InfoWindow();
-var marker;
 var map;
 var markersArray = [];
 
-function initialize() {
-    var mapOptions = {
-        zoom: 5,
-//        center: new google.maps.LatLng(-25.363882, 131.044922),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+var doGeolocation = function() {
     geocoder = new google.maps.Geocoder();
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+    if (false) {
+//    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = new google.maps.LatLng(position.coords.latitude,
                 position.coords.longitude);
-            geocoder.geocode({'latLng': pos}, function(results, status) {
+            geocoder.geocode({'latLng': pos}, function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[1]) {
-                        marker = new google.maps.Marker({
+                    if (results[2]) {
+                        var marker = new google.maps.Marker({
                             position: pos,
                             map: map
                         });
                         markersArray.push(marker);
-                        infowindow.setContent(results[1].formatted_address);
+                        infowindow.setContent(results[2].formatted_address);
                         infowindow.open(map);
+
+
                     } else {
                         alert('No results found');
                     }
@@ -40,30 +35,37 @@ function initialize() {
                 }
             });
             map.setCenter(pos);
-        }, function() {
+        }, function () {
             map.setCenter(new google.maps.LatLng(-25.363882, 131.044922));
         });
     } else {
         // Browser doesn't support Geolocation
         handleNoGeolocation(false);
     }
+}
 
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-        title: 'Select a City'
-    });
+function initialiseMaps() {
+    var mapOptions = {
+        zoom: 2,
+//        center: new google.maps.LatLng(-25.363882, 131.044922),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    doGeolocation();
 
     google.maps.event.addListener(map, 'click', function(e) {
         placeMarker(e.latLng, map);
     });
 
 }
+
+
+
 function placeMarker(position, map) {
     var lat = position.lat();
     var lng = position.lng();
-    console.log(position.lat(), position.lng());
     var pos = new google.maps.LatLng(lat, lng);
 
     var latlng = new google.maps.LatLng(lat, lng);
@@ -72,53 +74,53 @@ function placeMarker(position, map) {
         if (status == google.maps.GeocoderStatus.OK) {
             if (results[2]) {
 //                map.setZoom(11);
-                console.log(results);
-                console.log(results[2]);
-                console.log(results[2].address_components[1]);
                 if (results[2].address_components[1]) {
-                    marker = new google.maps.Marker({
+                    var marker = new google.maps.Marker({
                         position: latlng,
                         map: map
                     });
                     markersArray.push(marker);
-                    console.log(results)
-                    var localResults = results[1];
-                    var var1 = localResults.address_components[0].long_name;
-                    var var2 = localResults.address_components[1].long_name;
-                    var var3 = localResults.address_components[2].long_name;
-                    var var4 = localResults.address_components[3].long_name;
+                    var localResults = _.map(results[2].address_components, function(component) { return component.long_name });
+                    if (localResults.length >= 4) {
+                        var city = localResults[localResults.length - 3];
+                        getCityData(city, function(err) {
+                            if (!err) {
+                                infowindow.setContent(city);
+                                infowindow.open(map, marker);
 
-                    //console.log(city)
-//                    console.log(city)
-//                    console.log(getCityData(city));
-                    infowindow.setContent(var1 + "<br/>" + var2 + "<br/>" + var3 + "<br/>" + var3 + "<br/>");
-                    infowindow.open(map, marker);
+                                map.setCenter(pos);
+
+                                map.panTo(position);
+                            }
+                            else {
+                                alert(err);
+                            }
+                        });
+                    }
+                    else {
+                        console.log('No results found');
+                    }
                 }
             } else {
-                alert('No results found');
+                console.log('No results found');
             }
         } else {
-            alert('Geocoder failed due to: ' + status);
+            console.log('Geocoder failed due to: ' + status);
         }
     });
-
-//    map.setCenter(pos);
-
-//        map.panTo(position);
 }
 
-function clearOverlays() {
+var clearOverlays = function() {
     for (i in markersArray) {
         markersArray[i].setMap(null);
     }
     markersArray = [];
 }
 
-function handleNoGeolocation(errorFlag) {
+var handleNoGeolocation = function(errorFlag) {
+    var content = "";
     if (errorFlag) {
         var content = 'Error: The Geolocation service failed.';
-    } else {
-        var content = 'Error: Your browser doesn\'t support geolocation.';
     }
 
     var options = {
@@ -127,11 +129,72 @@ function handleNoGeolocation(errorFlag) {
         content: content
     };
 
-    var infowindow = new google.maps.InfoWindow(options);
+//    var infowindow = new google.maps.InfoWindow(options);
     map.setCenter(options.position);
 }
 
 
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var getCityData = function(city, func) {
+    $.get(sparqlQuery(city), function(result) { processResults(result, func); });
+}
+
+var sparqlQuery = function(city) {
+    var query = "http://dbpedia.org/sparql?query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns/>PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema/>SELECT * WHERE { <http://dbpedia.org/resource/%city%> ?p ?o . }&format=json"
+    query = query.replace("%city%", encodeURIComponent(encodeURIComponent(city)));
+    // Needs to be doubly encoded
+    return query;
+}
+
+var processResults = function(result, func) {
+    var ignorePredicates = [
+        "http://www.w3.org/2000/01/rdf-schema#label",
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "http://www.w3.org/2002/07/owl#sameAs",
+        "http://purl.org/dc/terms/subject"
+    ];
+    var preferredLanguage = "en";
+    var list = result.results.bindings;
+    if (list && !_.isUndefined(list)) {
+
+
+        $('#city-contents').empty();
+        _.forEach(list, function(predicate) {
+            var o = predicate.o, p = predicate.p;
+            var display = true;
+            if (_.indexOf(ignorePredicates, p.value) > -1)
+                display = false;
+            if (o['xml:lang'] && o['xml:lang'] != preferredLanguage)
+                display = false;
+            if (display)
+                displayLine(p, o);
+        });
+    }
+    else {
+        func("No results found");
+    }
+}
+
+var stripUrl = function(url) {
+    var hash = url.lastIndexOf('#');
+    var slash = url.lastIndexOf('/');
+    var pos = (hash > slash ? hash : slash);
+    if (pos > -1) {
+        url = url.substring(pos + 1);
+    }
+    return url;
+};
+
+var displayLine = function(p, o) {
+    var dispO = o.value, dispP = stripUrl(p.value);
+    var objLink = dispO;
+    if (o.value.indexOf('http') == 0)
+        objLink = '<a href="' + dispO + '" target="_blank">' + dispO + '</a>';
+    $('#city-contents').append('<tr><td>' + dispP + '</td><td>'+ objLink + '</td></tr>');
+
+};
+
+
+
+google.maps.event.addDomListener(window, 'load', initialiseMaps);
